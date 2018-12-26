106d6ec1fe8e6e3e743af6d56843626e5c6bbb82
screamcha <scream1234540@gmail.com>
2018-12-26 14:27:32 +0300

    add id generator

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NodeEditor from './NodeEditor'
 import EdgeEditor from './EdgeEditor'
 import ArrowLegend from '../ArrowLegend'
+import { ID } from '../../utils/idGenerator'
 
 import './styles.css'
 
@@ -36,7 +37,7 @@ class Dashboard extends Component {
     element.add(
       {
         data: {
-          id: Math.random(),
+          id: ID(),
           title: newNodeTitle
         },
         position: {
