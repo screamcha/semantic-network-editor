4fed0a1c6bd1b5ab8553d24cb2f058621b470c30
screamcha <scream1234540@gmail.com>
2019-05-28 22:52:41 +0300

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,16 @@ import React from "react";
 import "./Header.scss";
 
 const Header = props => {
-  return <header className="header">Grapher</header>;
+  return (
+    <header className="header">
+      <div className="header__content">
+        <span className="header__logo">Grapher</span>
+        <ul className="header__buttons">
+          <li className="header__button">Sign in</li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
